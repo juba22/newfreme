5d4c8c06bb49e7589df07665bd48c04b68711fbe
unknown <mjopa84@gmail.com>
2020-05-25 13:30:07 +0200

    some edite

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,49 @@
-/* global, $ */
+// global, $
+
 $(function () {
     "use strict";
+    
+    // information sec 
     
     $('.info-bar li').click(function () {
         $(this).addClass('selected').siblings().removeClass('selected');
         $('.info-contact div').hide();
         $('.' + $(this).data('name')).fadeIn();
     });
+    
+    // code button to up 
+    
+    $(window).scroll(function () {
+        
+        if($(window).scrollTop() >= 800){
+            
+            if($('.up').is(':hidden')){
+                
+                $('.up').fadeIn(500)
+            }
+        }else{
+                $('.up').fadeOut(500)
+            }
+    })
+    
+    $('.up').on('click', function(e) {
+            e.preventDefault()
+        
+            $('html , body').animate({
+                
+                scrollTop : 0
+                
+            },1000)
+        })
+    
+    // window scrolling smooth
+    
+    $('.nav li a').click(function (e) {
+        e.preventDefault()
+        $('html , body').animate({
+            scrollTop : $('.' + $(this).data('nav')).offset().top
+        }, 1500)
+    })
+    
     
 });
